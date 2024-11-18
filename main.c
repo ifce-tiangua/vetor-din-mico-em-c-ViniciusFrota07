8dda471db4cda5fbd14bcc705f26102fa440bc1c
ViniciusFrota07 <vfrota178@gmail.com>
2024-11-18 19:52:21 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("Hello");
+int main()
+{
+    int tam;
+    scanf("%d", &tam);
+    int *vetor = (int *)malloc(tam * sizeof(int));
 
+    for (int i = 0; i < tam; i++)
+    {
+        scanf("%d", &vetor[i]);
+    }
+    printf("[");
+    for (int i = 0; i < tam; i++)
+    {
+        printf("%d", vetor[i]);
+        if(i<tam-1){
+            printf(", ");
+    
+        }
+    }
+    printf("]");
+    free(vetor);
 }
